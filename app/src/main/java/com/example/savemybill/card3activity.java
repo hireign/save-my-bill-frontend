587a53c392d6a30b0fda14260e8cdf894f1d3f5e
Amogh Adithya Bangalore Lokanath <amoghadithyabl@gmail.com>
2020-04-05 03:24:27 +0530

    Added layouts

---
+++ app/src/main/java/com/example/savemybill/card3activity.java
@@ -0,0 +1,13 @@
+package com.example.savemybill;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class card3activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.card3activity);
+    }
+}
